f51961704e62273bd33da1cbdc71773156f72a13
joeatitagain <anetojoseph@gmail.com>
2026-03-26 11:18:46 +0100

    meta data issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: "Rexon Dev | Professional Software Development",
     description: "Transform your business with modern software solutions",
-    images: ['/twitter-image.png'],
+    images: ['/og-image.png'],
   },
   robots: {
     index: true,
@@ -65,7 +65,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
-
+        <link rel="icon" href="/favicon.ico"/>
+        <link rel="shortcut icon" href="/favicon.ico"/>
       </head>
       <body>
         <QuoteDialogProvider>
